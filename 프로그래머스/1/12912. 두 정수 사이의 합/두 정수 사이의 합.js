8f87ad9b79ef13c1635fba6c4975d2036bf58acb
JiMin Kim <142903244+gggimmmin@users.noreply.github.com>
2024-01-31 12:54:44 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    let start = Math.min(a,b);
+    let end = Math.max(a,b);
+    let sum = 0;
+    
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    
+    return sum;
+}

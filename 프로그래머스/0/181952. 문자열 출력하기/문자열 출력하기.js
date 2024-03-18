3675a23bd16dd201f0670cacd1fef0b2926e5c9d
JiMin Kim <142903244+gggimmmin@users.noreply.github.com>
2024-03-18 21:22:02 +0900

    [level 0] Title: 문자열 출력하기, Time: 58.18 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = ["HelloWorld!"];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    let str = input[0];
+    console.log(str);
+});
+
+
